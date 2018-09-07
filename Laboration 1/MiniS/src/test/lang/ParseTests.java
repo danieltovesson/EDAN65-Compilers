54224277f373138fdcbcd798288d743e66f76bcf
Daniel Tovesson <daniel.tovesson@me.com>
2018-09-07 16:04:04 +0200

    3 Add MiniS project

---
+++ Laboration 1/MiniS/src/test/lang/ParseTests.java
@@ -0,0 +1,18 @@
+package lang;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+
+import static org.junit.Assert.fail;
+
+public class ParseTests {
+	/** Directory where the test input files are stored. */
+	private static final File TEST_DIRECTORY = new File("testfiles");
+
+	@Test public void identifier() {
+		Util.testValidSyntax(TEST_DIRECTORY, "exampleprogram.in");
+	}
+}
