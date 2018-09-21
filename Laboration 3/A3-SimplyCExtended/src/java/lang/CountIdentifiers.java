1b8d637f64b60cb80945c5e025efb594b3b4be04
Daniel Tovesson <daniel.tovesson@me.com>
2018-09-21 14:00:43 +0200

    Fix MSN functionallity

--- Laboration 3/A3-SimplyCExtended/src/java/lang/CountIdentifiers.java
+++ Laboration 3/A3-SimplyCExtended/src/java/lang/CountIdentifiers.java
@@ -4,21 +4,43 @@ import lang.ast.*;
 
 class CountIdentifiers extends TraversingVisitor {
 
-  static int count = 0;
-  static int result(Program root){
+  static int currentDepth;
+  static int maxDepth;
+
+  static int result(Program root) {
+    currentDepth = 0;
+    maxDepth = 0;
     root.accept(new CountIdentifiers(), null);
-    return count;
+    return maxDepth;
   }
 
-  public Object visit(IfStmt node, Object data) {
+  public Object visit(Function node, Object data) {
+    currentDepth++;
     super.visit(node, data);
-	  count++;
+    if (currentDepth > maxDepth) {
+      maxDepth = currentDepth;
+    }
+    currentDepth--;
+    return null;
+	}
+
+  public Object visit(IfStmt node, Object data) {
+    currentDepth++;
+    super.visit(node, data);
+    if (currentDepth > maxDepth) {
+      maxDepth = currentDepth;
+    }
+    currentDepth--;
     return null;
 	}
 
   public Object visit(WhileStmt node, Object data) {
+    currentDepth++;
     super.visit(node, data);
-	  count++;
+    if (currentDepth > maxDepth) {
+      maxDepth = currentDepth;
+    }
+    currentDepth--;
     return null;
 	}
 }
