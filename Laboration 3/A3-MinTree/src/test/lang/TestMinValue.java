eeb9c62682963013aece6ac26181cbc7b4ed3275
Daniel Tovesson <daniel.tovesson@me.com>
2018-09-25 15:45:11 +0200

    Add test case for previous exercise

--- Laboration 3/A3-MinTree/src/test/lang/TestMinValue.java
+++ Laboration 3/A3-MinTree/src/test/lang/TestMinValue.java
@@ -17,14 +17,14 @@ public class TestMinValue {
 		Pair p1 = new Pair(l2, l3);
 		Pair p2 = new Pair(l1, p1);
 		Program p = new Program(p2);
-		
+
 		assertEquals(1, l1.localMin());
 		assertEquals(2, l2.localMin());
 		assertEquals(3, l3.localMin());
 		assertEquals(2, p1.localMin());
 		assertEquals(1, p2.localMin());
 	}
-	
+
 	@Test
 	public void globalMin() {
 		Leaf l1 = new Leaf(1);
@@ -33,14 +33,14 @@ public class TestMinValue {
 		Pair p1 = new Pair(l2, l3);
 		Pair p2 = new Pair(l1, p1);
 		Program p = new Program(p2);
-		
+
 		assertEquals(1, l1.globalMin());
 		assertEquals(1, l2.globalMin());
 		assertEquals(1, l3.globalMin());
 		assertEquals(1, p1.globalMin());
 		assertEquals(1, p2.globalMin());
 	}
-	
+
 	@Test
 	public void isMinValue() {
 		Leaf l1 = new Leaf(1);
@@ -49,12 +49,12 @@ public class TestMinValue {
 		Pair p1 = new Pair(l2, l3);
 		Pair p2 = new Pair(l1, p1);
 		Program p = new Program(p2);
-		
+
 		assertTrue(l1.isMinValue());
 		assertFalse(l2.isMinValue());
 		assertFalse(l3.isMinValue());
 	}
-	
+
 	@Test
 	public void nbrOfMinValues() {
 		Leaf l1 = new Leaf(1);
@@ -63,8 +63,20 @@ public class TestMinValue {
 		Pair p1 = new Pair(l2, l3);
 		Pair p2 = new Pair(l1, p1);
 		Program p = new Program(p2);
-		
+
 		assertEquals(1, p.nbrOfMinValues());
+	}
+
+	@Test
+	public void nbrOfMinValuesLargerThanOne() {
+		Leaf l1 = new Leaf(1);
+		Leaf l2 = new Leaf(1);
+		Leaf l3 = new Leaf(1);
+		Pair p1 = new Pair(l2, l3);
+		Pair p2 = new Pair(l1, p1);
+		Program p = new Program(p2);
+
+		assertEquals(3, p.nbrOfMinValues());
 	}
 
 }
